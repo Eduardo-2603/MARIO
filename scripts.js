0eb470a57eba0de77e61510687578fd7f2bd33a4
Eduardo-2603 <eduardo.comercial30@gmail.com>
2024-10-16 21:21:25 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function CliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function CliqueMascara(){
+    form.style.left = "-300px"
+    form.style.transform = "translatex(0)"
+    mascara.style.visibility = "hidden"
+}
